bbe31afad57f00d6c2fd070b2ff009c36ce73003
stefan9x <stefanj94@live.com>
2019-04-06 01:42:43 +0200

    small changes

--- vezba04/z3.py
+++ vezba04/z3.py
@@ -15,7 +15,7 @@ def CountingSort(A, B, k):
     for i in range(1, k):
         C[i] += C[i - 1]
 
-    for j in reversed(range(len(A))):
+    for j in range(len(A)):
         B[C[A[j]] - 1] = A[j]
         C[A[j]] -= 1
 
