36c79026b2bcc2255eed59e4c39f3d57b02c55fe
stefan9x <stefanj94@live.com>
2019-04-05 23:12:54 +0200

    Fixed bubble sort

--- vezba02/z2.py
+++ vezba02/z2.py
@@ -9,15 +9,14 @@ def rand_list(min, max, num_of_el):
     return list
 
 def bubble_sort(niz):
-    for i in range(0, len(niz)-1):
+    for i in range(0, len(niz)):
         for j in range(0, len(niz)-i-1):
             if niz[j] > niz[j + 1]:
-                tmp = niz[j + 1]
-                niz[j + 1] = niz[j]
-                niz[j] = tmp
-
+                niz[j], niz[j + 1] = niz[j + 1], niz[j]
+                               
 if __name__ == "__main__":
     A = rand_list(0, 20, 10)
+    
     print("Niz prije sortiranja:", A)
 
     start_time = time.perf_counter()
